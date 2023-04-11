41ee011f9cbb621691b75dc13f19bc0f8ca3e82c
Kendric285 <terrykendric@gmail.com>
2023-04-11 17:06:13 -0400

    Able to shoot and bounce of walls

--- classes.py
+++ classes.py
@@ -2,11 +2,28 @@
 from cmu_graphics import *
 import math, copy, decimal
 from functions import *
+#4 hours on 4/10
 
 class TankGame:
-    def __init__(self,player,enemies):
+    def __init__(self,player,enemies, balls):
         self.player = player
         self.enemies = enemies
+
+class Ball:
+    def __init__(self,x,y,contacts,angle,plusX,plusY):
+        self.x = x
+        self.y = y
+        self.contacts = contacts
+        self.angle = angle
+        self.plusX = plusX
+        self.plusY = plusY
+    def updateXY(self):
+
+        self.x += 4
+        self.y += 4
+
+    def __repr__(self):
+        return f'Ball x:{self.x}, y:{self.y}, angle:{self.angle}'
     
 
 class Player:
@@ -19,54 +36,39 @@ class Player:
         self.body_direction = body_direction
         self.mouseX = mouseX
         self.mouseY = mouseY
+        self.tankAddX, self.tankAddY = lcoor(self.x, self.y, self.mouseX, self.mouseY)
     def drawPlayer(self):
         theta = math.radians(self.body_direction - 45)
-
+        self.bullet_direction = theta
         frontX1 = (self.x + (50/2) * math.sin(theta))
         frontY1 = (self.y - (50/2) * math.cos(theta))
         frontX2 = (self.x + (50/2) * math.cos(theta))
         frontY2 = (self.y + (50/2) * math.sin(theta))
 
-        tankX = getLineCoorX(self.x,self.y,self.mouseX,self.mouseY)
-        tankY = getLineCoorY(self.x,self.y,self.mouseX,self.mouseY)
+
+        self.tankAddX, self.tankAddY = lcoor(self.x, self.y, self.mouseX, self.mouseY)
+
+        # tankX = getLineCoorX(self.x,self.y,self.mouseX,self.mouseY)
+        # tankY = getLineCoorY(self.x,self.y,self.mouseX,self.mouseY)
         #wheels, top square, 
         drawRect(self.x,self.y,65,45,rotateAngle = self.body_direction,fill = "grey", align = "center")
         drawRect(self.x,self.y,50,50,rotateAngle = self.body_direction,fill = "black", align = "center")
         drawLine(frontX1,frontY1,frontX2,frontY2,fill = 'red', lineWidth = 5)
-        drawLine(self.x,self.y,self.x + tankX, self.y + tankY, fill = 'orange', lineWidth = 10)
+        drawLine(self.x,self.y,self.x + self.tankAddX, self.y + self.tankAddY, fill = 'orange', lineWidth = 10)
         drawCircle(self.x,self.y,10, fill = 'grey',align= "center")
-        
-def getLineCoorX(x1,y1,mouseX,mouseY):
-    hypotenuse = distance(x1,y1,mouseX,mouseY)
-    if mouseY > y1:
-        adjacent = mouseY - y1
-        theta = math.acos(adjacent/hypotenuse)
-        xAdd = 30 * math.sin(theta)
-
-        return xAdd if mouseX > x1 else (-xAdd)
-
-    elif y1 > mouseY:
-        opposite = y1 - mouseY
-        theta = math.asin(opposite/hypotenuse)
-        xAdd = 30 * math.cos(theta)
-
-        return xAdd if mouseX > x1 else (-xAdd)
-    return 0
-def getLineCoorY(x1,y1,mouseX,mouseY):
-    hypotenuse = distance(x1,y1,mouseX,mouseY)
-    if mouseY > y1:
-        adjacent = mouseY - y1
-        theta = math.acos(adjacent/hypotenuse)
-        yAdd = 30 * math.cos(theta)
-
-        return yAdd
-
-    elif y1 > mouseY:
-        opposite = y1 - mouseY
-        theta = math.asin(opposite/hypotenuse)
-        yAdd = 30 * math.sin(theta)
-
-        return (-yAdd)
-    return -30 if mouseX < x1 else 30
-   
+    def endOfBarrel(self):
+        tankX = self.x + self.tankAddX
+        tankY = self.y + self.tankAddY
+        return (tankX,tankY)
     
+# class Enemy:
+#     def __init__(self,x,y,bullet_direction,body_direction,mouseX, mouseY):
+#         self.lives = 
+
+def lcoor(x1,y1,mouseX,mouseY):
+    r = distance(x1,y1,mouseX,mouseY)
+    dx = mouseX - x1
+    dy = mouseY - y1
+    return (((dx / r) * 30), ((dy / r) * 30))
+
+
--- tanks.py
+++ tanks.py
@@ -8,11 +8,41 @@ def onAppStart(app):
     app.playerX = app.width/2
     app.playerY = app.height/2
     app.player = Player(3,10,app.width/2,app.height/2,0,0,0,0)
-    app.game = TankGame(app.player,[])
+    app.game = TankGame(app.player,[],[])
+    app.balls = []
+    app.step = 0
 
 def onMouseMove(app, mouseX, mouseY):
     app.player.mouseX = mouseX
     app.player.mouseY = mouseY
+
+def onMousePress(app, mouseX, mouseY):
+    ballX,ballY = app.player.endOfBarrel()
+    app.balls.append(Ball(ballX,ballY,0,app.player.body_direction,app.player.tankAddX, app.player.tankAddY))
+    
+def onStep(app):
+    if app.balls != []:
+        checkBallCollision(app, app.balls)
+        updateBallPosition(app.balls)
+    
+def updateBallPosition(balls):
+    for ball in balls:
+        ball.x += ball.plusX
+        ball.y += ball.plusY
+def checkBallCollision(app,balls):
+    for ball in balls:
+        if (ball.x < 5 ) or (ball.x > app.width - 5):
+            ball.contacts += 1
+            if ball.contacts > 1:
+                app.balls.remove(ball)
+            else:
+                ball.plusX = ball.plusX * (-1)
+        if (ball.y > app.height - 5) or (ball.y < 5):
+            ball.contacts += 1
+            if ball.contacts > 1:
+                app.balls.remove(ball)
+            else:
+                ball.plusY = ball.plusY * (-1)
 
 def onKeyPress(app,key):
     return True
@@ -21,19 +51,24 @@ def onKeyHold(app,keys):
     angle = app.player.body_direction
     radians = math.radians(angle)
     if 'w' in keys:
-        app.player.y -= (10 * math.cos(radians))
-        app.player.x += (10 * math.sin(radians))
-        print(app.player.y)
+        app.player.y -= (20 * math.cos(radians))
+        app.player.x += (20 * math.sin(radians))
     elif 's' in keys:
-        app.player.y += (10 * math.cos(radians))
-        app.player.x -= (10 * math.sin(radians))
+        app.player.y += (20 * math.cos(radians))
+        app.player.x -= (20 * math.sin(radians))
     if 'd' in keys:
-        app.player.body_direction += 5
+        app.player.body_direction += 10
     elif 'a' in keys:
-        app.player.body_direction -= 5
+        app.player.body_direction -= 10
+
+def drawBalls(balls):
+    for ball in balls:
+        drawCircle(ball.x, ball.y, 5, fill = "red")
 
 def redrawAll(app):
     app.player.drawPlayer()
+    drawBalls(app.balls)
+
 
 def main():
     runApp()
